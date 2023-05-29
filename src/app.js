b17a118de057db91923022b41819910b169a1e62
Tuchix <tuchix10@gmail.com>
2023-05-29 19:13:14 -0300

    Primer commit de la primera entrega

--- src/app.js
+++ src/app.js
@@ -1,25 +1,16 @@
 import express from 'express';
-import { ProductManager } from './productManager.js'
+import productsRouter from './routes/products.router.js';
+import cartsRouter from './routes/carts.router.js';
 
 const app = express();
-const listaProductos = new ProductManager();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
 
 app.get('/', (req, res) => {
     res.send('<h1> Bienvenido <h1>')
 }) ,
-
-app.get('/products', (req, res) => {
-    let limit = req.query.limit;
-    if(!limit) {
-        res.send(listaProductos.getProducts())
-    }
-    res.send(listaProductos.getProductLimit(limit));
-});
-
-app.get('/products/:pid', (req, res) => {
-    let idProducto = Number(req.params.pid);
-    res.send(listaProductos.getProductById(idProducto))
-});
 
 app.listen(8080, () => {
     console.log('servidor escuchando en el puerto 8080')
---
+++ src/routes/products.router.js
@@ -0,0 +1,59 @@
+import { Router } from "express";
+import { ProductManager } from "../productManager.js";
+
+const router = Router();
+const listaProductos = new ProductManager();
+
+router.get('/', (req, res) => {
+    let limit = req.query.limit;
+    if(!limit) {
+        res.send({status : "success", payload : listaProductos.getProducts()})
+    }
+    res.send({status : "success", payload : listaProductos.getProductLimit(limit)});
+});
+
+router.post('/', (req, res) => {
+    const newProduct = req.body;
+    try {
+        listaProductos.addProduct(newProduct);
+        res.status(201).send({status : "success", payload : "Producto agregado con exito"})
+    }
+    catch {
+        res.status(400).send({status : "Error", error: "El código ingresado ya se encuentra en la base de datos"})
+    }
+});
+
+router.get('/:pid', (req, res) => {
+    let idProducto = Number(req.params.pid);
+    try {
+        res.send({status : "success", payload : listaProductos.getProductById(idProducto)});
+    }
+    catch {
+        res.status(404).send({status : "Error", error: "Producto no encontrado"})
+    }
+});
+
+router.put('/:pid', (req, res) => {
+    let idProducto = Number(req.params.pid);
+    let dataToUpdate = req.body
+    try {
+        listaProductos.updateProduct(idProducto, dataToUpdate)
+        res.send({status : "success", payload : "Producto actualizado correctamente"});
+    }
+    catch {
+        res.status(400).send({status : "Error", error: "Producto no encontrado o propiedades vacias"})
+    }
+});
+
+router.delete('/:pid', (req, res) => {
+    let idProducto = Number(req.params.pid);
+    try {
+        listaProductos.deleteProduct(idProducto)
+        res.send({status : "success", payload : "Producto borrado"});
+    }
+    catch {
+        res.status(400).send({status : "Error", error: "Producto no encontrado"})
+    }
+});
+
+export default router;
